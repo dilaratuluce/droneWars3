334f6173c724dbea1b3848b25032b37942885b68
dilaratuluce <dilaratuluce@gmail.com>
2023-07-27 14:32:15 +0300

    coming health plus

--- Drone Wars/Assets/Scripts/healthPlus.cs
+++ Drone Wars/Assets/Scripts/healthPlus.cs
@@ -6,8 +6,16 @@ public class healthPlus : MonoBehaviour
 {
     Vector3 startPosition;
     Vector3 endPosition;
-    float desiredDuration = 2f;
+    float desiredDuration = 1f;
     float elapsedTime;
+
+    bool comingToPlayer;
+    Vector3 comingStartPosition;
+    Vector3 comingEndPosition;
+    float comingDesiredDuration = 0.5f;
+    float comingElapsedTime;
+    referancePoint comingPosition;
+    Vector3 comingVector;
 
     // Start is called before the first frame update
     void Start()
@@ -15,19 +23,42 @@ public class healthPlus : MonoBehaviour
         startPosition = transform.position;
         endPosition = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
 
+        comingToPlayer = false;
+
     }
 
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        float percentageComplete = elapsedTime / desiredDuration;
-
-        transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete);
-
-        if (transform.localPosition.y >= startPosition.y+3 - 0.1)
+        if (!comingToPlayer)
         {
-            Destroy(this.gameObject);
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / desiredDuration;
+
+            transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete);
+
+            if (transform.localPosition.y >= startPosition.y + 3 - 0.1)
+            {
+                //Destroy(this.gameObject);
+                comingToPlayer = true;
+                comingStartPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                comingEndPosition = new Vector3();
+                comingElapsedTime = 0;
+                comingPosition = FindObjectOfType<referancePoint>();
+                comingVector = comingPosition.getPosition();
+            }
         }
+        else
+        {
+            comingElapsedTime += Time.deltaTime;
+            float comingPercentageComplete = comingElapsedTime / comingDesiredDuration;
+
+            transform.position = Vector3.Lerp(comingStartPosition, comingEndPosition, comingPercentageComplete);
+            if (transform.localPosition.y >= endPosition.y - 0.1)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        
 
     }
 }
