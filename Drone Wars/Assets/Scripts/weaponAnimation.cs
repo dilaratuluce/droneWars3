989c1b1bfe4d4865f8497db82b30d17d879f4184
dilaratuluce <dilaratuluce@gmail.com>
2023-08-13 17:09:15 +0300

    rocket launcher

--- Drone Wars/Assets/Scripts/weaponAnimation.cs
+++ Drone Wars/Assets/Scripts/weaponAnimation.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 public class weaponAnimation : MonoBehaviour
 {
+
+    gun my_gun;
     // Start is called before the first frame update
     void Start()
     {
         transform.localRotation = Quaternion.identity;
+        my_gun = FindObjectOfType<gun>();
     }
 
     public void Open()
     {
-        LeanTween.rotateLocal(gameObject, new Vector3(gameParameters.weapon_rotation, 0, 0), gameParameters.weapon_animation_duration).setEaseOutBack().setOnComplete(Close);
+        LeanTween.rotateLocal(gameObject, new Vector3(gameParameters.weapon_rotation, 0, 0), my_gun.getReloadTime()).setEaseOutBack().setOnComplete(Close);
     }
 
     public void Close()
     {
-        LeanTween.rotateLocal(gameObject, Vector3.zero, gameParameters.weapon_animation_duration).setEaseInBack();
+        LeanTween.rotateLocal(gameObject, Vector3.zero, my_gun.getReloadTime()).setEaseInBack();
     }
 }
