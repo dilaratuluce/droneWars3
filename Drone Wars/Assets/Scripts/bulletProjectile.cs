3229798e1e8eb0e3ab567fb5e76cf914e2c40d49
dilaratuluce <dilaratuluce@gmail.com>
2023-08-14 22:38:06 +0300

    shotgun

--- Drone Wars/Assets/Scripts/bulletProjectile.cs
+++ Drone Wars/Assets/Scripts/bulletProjectile.cs
@@ -63,15 +63,6 @@ public class bulletProjectile : MonoBehaviour
         my_gun = FindObjectOfType<gun>();
         takingGun = FindObjectOfType<TakingGun>();
 
-        /*Vector3 mouseWorldPosition = Vector3.zero;
-        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderMask))
-        {
-            mouseWorldPosition = raycastHit.point;
-        }
-
-        Vector3 aimDir = (mouseWorldPosition - BulletSpawnPoint.position).normalized; */
         gameObject.transform.position = BulletSpawnPoint.position;
         gameObject.transform.rotation = my_gun.getRotation();
         Quaternion spreadRotation = Quaternion.Euler(spread_value, 0, 0);
